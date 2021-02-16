b8e4455dab0553b60f380709227066780ca2c8c8
morioka1206 <r.morioka@icloud.com>
2021-02-16 12:47:21 +0900

    メモ投稿機能のフロントエンド実装""
    This reverts commit 508fbbc6d14878c151658dc2d94257d12157a5de.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
 
 
   def create
-    post = Post.create(content: params[:content], checke: false)
+    post = Post.create(content: params[:content], checked: false)
     render json:{ post: post }
   end
 
